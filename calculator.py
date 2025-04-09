f4ee301f173159731c1987ab70c9cf41bf62a2b9
kian-hidalgo-uf <kian.hidalgo@ufl.edu>
2025-04-09 12:41:07 -0400

    person2 KH

--- calculator.py
+++ calculator.py
@@ -4,8 +4,31 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
+
 # First example
 def add(a, b): 
-    pass
+    result = a + b
+    return result
+def sub(a, b):
+    result = a - b
+    return result
+def mul(a, b):
+    result = a * b
+    return result
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    result = b / a
+    return result
 
+def log(a, b):
+    if a <= 0 or b <= 0:
+        raise ValueError
+    result = math.log(a, b)
+    return result
+
+def exp(a, b):
+    result = a ** b
+    return result
 
