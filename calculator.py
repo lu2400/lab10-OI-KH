46b1234134ec09787bd16e6fb3f150ee8c358ad8
Lu <oluchiighodalo@gmail.com>
2025-04-09 12:54:26 -0400

    person 1 OI

--- calculator.py
+++ calculator.py
@@ -6,7 +6,14 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
+def square_root(a): 
+    if a < 0:
+        raise ValueError("ValueError")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b): a + b
 
 def subtract(a, b): a - b    
