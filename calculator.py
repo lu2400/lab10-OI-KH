f662513ad841c83a5a96443c540042f94f83701c
Lu <oluchiighodalo@gmail.com>
2025-04-09 13:54:17 -0400

    person 1 OI

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+# https://github.com/lu2400/lab10-OI-KH
+# Partner 1: Lu Ighodalo
+# Partner 2: Kian Hidalgo
+
 import math
 
 """
@@ -13,44 +17,39 @@ import math
 def add(a, b): 
     result = a + b
     return result
-def sub(a, b):
+def subtract(a, b):
     result = a - b
     return result
+
 def mul(a, b):
     result = a * b
     return result
+
 def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     result = b/a
     return result
 
-def log(a, b):
-    if a <= 0 or b <= 0:
-        raise ValueError
-    result = math.log(a, b)
+def square_root(a):
+    if a < 0:
+        raise ValueError("ValueError")
+    result = math.sqrt(a)
     return result
 
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def exp(a, b):
+    if a < 0:
+        raise ValueError("ValueError")
     result = a ** b
     return result
 
 def add(a, b): a + b
-
-
-def multiply(a, b): a * b
-
-def divide(a, b):
-    if a == 0:
-        raise ValueError("ZeroDivisionError")
-    return a / b
 
 def logarithm(a, b):
     if a <= 0 or b <= 0:
         raise ValueError("ValueError")
     return math.log(a, b)
 
-def exponent(a, b):
-    if a < 0 and b != int(b):
-        raise ValueError("ValueError")
-    return a ** b   
